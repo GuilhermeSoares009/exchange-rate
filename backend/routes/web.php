ebff7d4bffc63596a3d94b3ca20bb5979d2b359b
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-07-31 07:24:04 -0300

    Adicionando a parte do backend - Ajustando também arquivos desnecessários

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ExchangeRateController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/exchange-rate', [ExchangeRateController::class, 'exchangeRate']);
